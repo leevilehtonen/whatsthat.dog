aafb292dc8dff241679f12a004cd17ac4a5ab2d1
Leevi Lehtonen <leevi.k.lehtonen@gmail.com>
2019-05-24 21:43:27 +0300

    Updates

---
+++ precache-manifest.61409fdc915e39a05f1dfc5eae87f102.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5967f2a88e99d97e942bc3fbed436d69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44410b95ff4d3b887957",
+    "url": "/static/css/2.b176d3f8.chunk.css"
+  },
+  {
+    "revision": "f71af9fce4680b3a3a2a",
+    "url": "/static/css/main.85e01e40.chunk.css"
+  },
+  {
+    "revision": "44410b95ff4d3b887957",
+    "url": "/static/js/2.a5f84fb0.chunk.js"
+  },
+  {
+    "revision": "f71af9fce4680b3a3a2a",
+    "url": "/static/js/main.ab2eb6d4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f4913397c9e07b37b69bccb37eefd966",
+    "url": "/static/media/bg3.f4913397.png"
+  }
+]);
